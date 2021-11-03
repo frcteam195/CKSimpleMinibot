61ae4d8b083749070c034e55c9a3ad0816d42adc
Robert <robert.a.hilton.jr@gmail.com>
2021-11-03 02:00:01 -0400

    Update WiFiCredentials.hpp

--- include/WiFiCredentials.hpp
+++ include/WiFiCredentials.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+/*
+BEFORE EDITING THIS FILE
+----------------------------
+Make sure to run the following command from the repo root
+git update-index --assume-unchanged include/WiFiCredentials.hpp
+in order to prevent your WiFI credentials from being published online
+*/
 // WiFi network name and password:
 const char* netName = "WIFI_NAME";
 const char* netPswd = "********";
